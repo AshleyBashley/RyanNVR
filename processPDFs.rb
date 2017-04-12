809146e17f0357961c0275da9b7c9452deb5d3fe
AshleyBashley <woerner.ashley@gmail.com>
2017-04-12 15:35:12 -0400

    updated sink selections

--- processPDFs.rb
+++ processPDFs.rb
@@ -9,47 +9,55 @@ require 'ap' #gem install awesome_print
 ########################################################################
 ########################################################################
 
-##This method generates an order object from a RYAN home PDF.
-def generateObjectFromOrder_RYAN(pdfName)
+##This method generates an order object from a NV home PDF.
+def generateObjectFromOrder_NV(pdfName)
 	o={}
-	o[:communityType] = "Ryan Homes"
+	o[:communityType] = "NVHomes"
 	o[:fileName] = pdfName
 
 	reader = PDF::Reader.new(pdfName)
 
 	reader.pages.each{|x| #Iterate over each of the pages in the reader
 		x.text.split(/\n/).each{|y| #iterate over each line in the page
+			
 			#Parse the start date to determine sink
 			if y["Start Date"]
+				
 				o[:orderDate] = Date.strptime(y[y.index("Start Date")+10..-1], '%m/%d/%Y')
+			end
+			if y["Start Dat"] && o[:orderDate].nil?
+				o[:orderDate] = Date.strptime(y[y.index("Start Dat")+9..-1], '%m/%d/%Y')
 			end
 
 			#Parse the faucet fixtures from the doc
 			if y["FAUCET FIXTURES KITCHEN"]
 				if y["UPGRADE"]
-					o[:FaucetSpread] = 2316584651 #Fauced upgraded
+					o[:FaucetSpread] = 'faucet centered, soap 8" to R' #Faucet spread 
 				else
-					o[:FaucetSpread] = 9876235 #No faucet upgrade
+					o[:FaucetSpread] = 'faucet centered, handle to R, soap to R of handle' #Faucet upgrade
 				end
 			end
 
 
 			#Parse the Kitchen Sink fixtures from the doc
-			if y["KITCHEN SINK"] #If the current line contains "KITCHEN SINK"
-				if !y["UPGRADE"] #If the current line contains "UPGRADE"
-					if o[:orderDate] > Date.strptime('02/02/2017', '%m/%d/%Y') #If the orderDate is after 02/02/2017
+			if y["KFK"] #If the current line contains "KFK"
+				if !y["KFL"] #If the current line contains "KFL"
+					if o[:orderDate] > Date.strptime('02/14/2017', '%m/%d/%Y') #If the orderDate is after 02/14/2017
+						
 						o[:KitchenSink] = "AS342"
+					else
+						o[:KitchenSink] = "AS333"
+
+					if o[:KitchenSink] = "11409"
 					else
 						o[:KitchenSink] = "K3821-4"
 					end
-				else
 					o[:KitchenSink] = false
 				end
 			end
-		}
-	}
+	end
 	return o
-end
+}
 
 ########################################################################
 ########################################################################
@@ -57,7 +65,7 @@ end
 ########################################################################
 ########################################################################
 
-##This method generates an order object from an NV home PDF.
+##This method generates an order object from an RYAN home PDF.
 def generateObjectFromOrder_NV(pdfName)
 	#Not yet implemented.
 	return {}
@@ -68,21 +76,22 @@ def main
 	parsedOrders = []
 
 	#Parse all RYAN pdfs
-	Dir.mkdir('PDFs_RYAN') if !File.directory?('PDFs_RYAN')
-	Dir['PDFs_RYAN/*.pdf'].each{|x|
+	Dir.mkdir('PDFs_NV') if !File.directory?('PDFs_NV')
+	Dir['PDFs_NV/*.pdf'].each{|x|
 		#Generate the parsed order from the PDF and push it onto parsedOrders array.
-		parsedOrders << generateObjectFromOrder_RYAN(x)
+		parsedOrders << generateObjectFromOrder_NV(x)
 	}
 
 	# Parse all NV pdfs
-	# Dir.mkdir('PDFs_NV') if !File.directory?('PDFs_NV')
-	# Dir['PDFs_NV/*.pdf'].each{|x|
+	Dir.mkdir('PDFs_NV') if !File.directory?('PDFs_NV')
+	Dir['PDFs_NV/*.pdf'].each{|x|
 	# 	#Generate the parsed order from the PDF and push it onto parsedOrders array.
-	# 	parsedOrders << generateObjectFromOrder_NV(x)
-	# }
+	 	parsedOrders << generateObjectFromOrder_NV(x)
+	 }
 
 	#awesome_print out all of the orders we just processed!
 	ap parsedOrders
 end
-
+	}
 main
+end
